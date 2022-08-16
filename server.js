18ff258dafe45bcf6e56ca5ff4f8eb18ec40d114
Albertus Chua <albertus.chua@gmail.com>
2022-08-16 14:51:20 -0400

    Changes

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const MongoClient = require('mongodb').MongoClient;
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 require('dotenv').config();
 
 // DB string
